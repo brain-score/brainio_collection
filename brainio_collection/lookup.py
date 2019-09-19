c457740377bfd46536b27d0926732b8f92a1708e
Jonathan Prescott-Roy <jjpr@mit.edu>
2019-09-19 15:06:48 -0400

    Trying to switch to a lookup.db fetched from S3, but this approach introduces circular imports.  Need a way around that.

--- brainio_collection/lookup.py
+++ brainio_collection/lookup.py
@@ -3,8 +3,13 @@ import os
 
 import peewee
 
-_logger = logging.getLogger(__name__)
+from .fetch import get_fetcher
 
-lookupdb_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "lookup.db"))
+_logger = logging.getLogger(__name__)
+_db_unique_name = "lookup_db"
+_db_location = "https://brainio-collection.s3.amazonaws.com/lookup.db"
+_fetcher = get_fetcher("S3", _db_location, _db_unique_name)
+
+lookupdb_path = _fetcher.fetch()
 _logger.debug(f"Loading database from {lookupdb_path}")
 pwdb = peewee.SqliteDatabase(lookupdb_path)
