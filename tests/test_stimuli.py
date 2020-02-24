e16efe79735fcb73eeb2593a2e05f07418a0dbf5
Martin Schrimpf <m4rtinsch@gmail.com>
2020-02-24 16:24:55 -0500

    add 300/606 stimuli explanation

--- tests/test_stimuli.py
+++ tests/test_stimuli.py
@@ -53,5 +53,10 @@ def test_list_stimulus_set(stimulus_set):
 @pytest.mark.private_access
 def test_klab_Zhang2018search():
     stimulus_set = brainio_collection.get_stimulus_set('klab.Zhang2018.search_obj_array')
+    # There are 300 presentation images in the assembly but 606 in the StimulusSet (explanation from @shashikg follows).
+    # For each of the visual search task out of total 300, you need two images (one - the target image,
+    # second - the search space image) plus there are 6 different mask images to mask objects
+    # present at 6 different locations in a specified search image.
+    # Therefore, a total of 300 * 2 + 6 images are there in the stimulus set.
     assert len(stimulus_set) == 606
     assert len(set(stimulus_set['image_id'])) == 606
