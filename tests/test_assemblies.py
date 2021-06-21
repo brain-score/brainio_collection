5cbe194f60b9a7d96ad166dd7907f3e9c62db6b9
Martin Schrimpf <m4rtinsch@gmail.com>
2021-06-21 16:59:53 -0400

    add unit tests for V1 properties assemblies and stimulus_sets

--- tests/test_assemblies.py
+++ tests/test_assemblies.py
@@ -1,6 +1,5 @@
-import os
-
 import numpy as np
+import os
 import pytest
 import xarray as xr
 from PIL import Image
@@ -45,6 +44,12 @@ from brainio_collection import fetch
         'dicarlo.Rust2012.array',
         'dicarlo.BashivanKar2019.naturalistic',
         'dicarlo.BashivanKar2019.synthetic',
+        'movshon.Cavanaugh2002a',
+        'devalois.DeValois1982a',
+        'devalois.DeValois1982b',
+        'movshon.FreemanZiemba2013_V1_properties',
+        'shapley.Ringach2002',
+        'schiller.Schiller1976c',
 ))
 def test_list_assembly(assembly):
     l = brainio_collection.list_assemblies()
@@ -326,8 +331,43 @@ class TestRustArray:
     pytest.param('dicarlo.BashivanKar2019.naturalistic', (24320, 233, 1), 309760, marks=[pytest.mark.private_access]),
     pytest.param('dicarlo.BashivanKar2019.synthetic', (21360, 233, 1), 4319940, marks=[pytest.mark.private_access]),
 ])
-def test_synthetic(assembly, shape, nans):
+def test_BashivanKar2019(assembly, shape, nans):
     assy = brainio_collection.get_assembly(assembly)
     assert assy.shape == shape
     assert np.count_nonzero(np.isnan(assy)) == nans
 
+
+@pytest.mark.private_access
+class TestMarques2020V1Properties:
+    @pytest.mark.parametrize('identifier,num_neuroids,properties,stimulus_set_identifier', [
+        ('movshon.Cavanaugh2002a', 190, [
+            'surround_suppression_index', 'strongly_suppressed', 'grating_summation_field', 'surround_diameter',
+            'surround_grating_summation_field_ratio'],
+         'dicarlo.Marques2020_size'),
+        ('devalois.DeValois1982a', 385, [
+            'preferred_orientation'],
+         'dicarlo.Marques2020_orientation'),
+        ('devalois.DeValois1982b', 363, [
+            'peak_spatial_frequency'],
+         'dicarlo.Marques2020_spatial_frequency'),
+        ('movshon.FreemanZiemba2013_V1_properties', 102, [
+            'absolute_texture_modulation_index', 'family_variance', 'max_noise', 'max_texture', 'noise_selectivity',
+            'noise_sparseness', 'sample_variance', 'texture_modulation_index', 'texture_selectivity',
+            'texture_sparseness', 'variance_ratio'],
+         'movshon.FreemanZiemba2013_properties'),
+        ('shapley.Ringach2002', 304, [
+            'bandwidth', 'baseline', 'circular_variance', 'circular_variance_bandwidth_ratio', 'max_ac', 'max_dc',
+            'min_dc', 'modulation_ratio', 'orientation_selective', 'orthogonal_preferred_ratio',
+            'orthogonal_preferred_ratio_bandwidth_ratio', 'orthogonal_preferred_ratio_circular_variance_difference'],
+         'dicarlo.Marques2020_orientation'),
+        ('schiller.Schiller1976c', 87, [
+            'spatial_frequency_selective', 'spatial_frequency_bandwidth'],
+         'dicarlo.Marques2020_spatial_frequency'),
+    ])
+    def test_assembly(self, identifier, num_neuroids, properties, stimulus_set_identifier):
+        assembly = brainio_collection.get_assembly(identifier)
+        assert set(assembly.dims) == {'neuroid', 'neuronal_property'}
+        assert len(assembly['neuroid']) == num_neuroids
+        assert set(assembly['neuronal_property'].values) == set(properties)
+        assert assembly.stimulus_set is not None
+        assert assembly.stimulus_set.identifier == stimulus_set_identifier
